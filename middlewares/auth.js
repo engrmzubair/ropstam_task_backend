37a8ea7209954087f810a2f6fb5fb0d553c1b48c
engrmzubair <engrmzubair2@gmail.com>
2023-05-05 22:36:28 +0500

    auth middleware

---
+++ middlewares/auth.js
@@ -0,0 +1,20 @@
+const jwt = require('jsonwebtoken');
+const config = require('../config');
+
+function auth(req, res, next) {
+    const token = req.headers.authorization?.split(' ')[1];
+
+    if (!token) {
+        return res.status(401).json({ message: 'Missing authentication token' });
+    }
+
+    try {
+        const decoded = jwt.verify(token, config.jwtSecret);
+        req.userId = decoded.userId;
+        next();
+    } catch (err) {
+        return res.status(401).json({ message: 'Invalid authentication token' });
+    }
+}
+
+module.exports = auth;
